8c463acdc3c46cfaf6e12bf038326e17b5874783
Angel Leyes <angelleyesdk@gmail.com>
2024-04-23 20:52:35 -0300

    se creo el header y el footer
    se creo el header y el footer
    capeta img y css

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pokedex</title>
+
+    <link rel="stylesheet" href="./css/footer.css">
+</head>
+<body>
+<nav class="contenedor__form">
+
+    <div class="logo__titulo">
+        <img src="./img/Pokédex_logo.png" alt="logo__titulo">
+    </div>
+
+</nav>
+<footer>
+    <p>Martin Gonzales - Axel Ian - Angel Leyes</p>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+<nav class="contenedor__form">
+    <div class="logo">
+        <img src="./img/LogoPokeball.png" alt="logo">
+    </div>
+
+        <div class="logo__titulo">
+        <img src="./img/Pokédex_logo.png" alt="logo__titulo">
+        </div>
+
+
+            <form class="form" action="#" method="post">
+                <input
+                    type="text"
+                    placeholder="Usuario"
+                    id="username"
+                    class="form__input"
+                >
+
+                    <input
+                    type="password"
+                    placeholder="Contraseña"
+                    id="password"
+                    class="form__input"
+                    >
+                <button type="submit" class="form__input-button">Ingresar</button>
+
+
+</nav>
+</body>
+</html>
