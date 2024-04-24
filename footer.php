c01bb940d3f30554d714e2c5fde938062cba9985
Angel Leyes <angelleyesdk@gmail.com>
2024-04-23 23:42:51 -0300

    se modifico el footer
    se modifico el footer

--- footer.php
+++ footer.php
@@ -7,13 +7,6 @@
     <link rel="stylesheet" href="./css/footer.css">
 </head>
 <body>
-<nav class="contenedor__form">
-
-    <div class="logo__titulo">
-        <img src="./img/Pokédex_logo.png" alt="logo__titulo">
-    </div>
-
-</nav>
 <footer>
     <p>Martin Gonzales - Axel Ian - Angel Leyes</p>
 </body>
