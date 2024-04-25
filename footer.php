66a6de1ee0127df5f7a6d4472e91d86063260e2b
Angel Leyes <angelleyesdk@gmail.com>
2024-04-25 18:19:40 -0300

    se cambio de Rama
    se cambio de Rama

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pokedex</title>
+
+    <link rel="stylesheet" href="./css/footer.css">
+</head>
+<body>
+<footer>
+    <p>Martin Gonzales - Axel Ian - Angel Leyes</p>
+</body>
+</html>
